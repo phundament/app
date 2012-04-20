344edf689d6d07e051f888380ea63df3913c221d
Tobias Munk <schmunk@usrbin.de>
2012-04-20 17:19:58 +0200

    fixed filename

--- protected/config/console.p3.php
+++ protected/config/console.p3.php
@@ -3,7 +3,7 @@
 // This is the configuration for yiic console application.
 // Any writable CConsoleApplication properties can be configured here.
 
-$mainConfig = require('p3.php');
+$mainConfig = require('main.p3.php');
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
 	'name'=>'My Console Application',
