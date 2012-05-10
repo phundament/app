fb62602aecd66565aae44ac11cfad4fe8a5807b3
Tobias Munk <schmunk@usrbin.de>
2012-05-11 00:07:07 +0200

    added installation notice

--- install/P3Setup.php
+++ install/P3Setup.php
@@ -10,12 +10,19 @@ class P3Setup {
         $composer = $event->getComposer();
         // do stuff
         echo "Welcome to Phundament Installation 3 via composer\n\n";
+        echo "This setup script will download all packages specified in composer.json. It will also trigger the creation of an ".
+            "Yii web application, please answer the upcoming confirmation question with [y]es.\n\n";
+        if(self::confirm("Install Phundament 3 now?")) {
+
+        } else {
+            exit("Installation aborted.\n");
+        }
     }
 
     public static function postInstall(Event $event) {
         $composer = $event->getComposer();
         // do stuff
-        echo "\n\nInstallation completed.\n\nThank you for choosing Phundament 3!\n";
+        echo "\n\nInstallation completed.\n\nThank you for choosing Phundament 3!\n\n";
     }
 
     public static function preUpdate(Event $event) {
@@ -144,4 +151,19 @@ class P3Setup {
         $runner->run($args);
     }
 
+	/**
+	 * Asks user to confirm by typing y or n.
+     *
+     * Credits to Yii CConsoleCommand
+	 *
+	 * @param string $message to echo out before waiting for user input
+	 * @return bool if user confirmed
+	 *
+	 * @since 0.5
+	 */
+	public static function confirm($message)
+	{
+		echo $message.' [yes|no] ';
+		return !strncasecmp(trim(fgets(STDIN)),'y',1);
+	}
 }
