75844f9884785bd946ff77a4c75c48225c46bcef
Tobias Munk <schmunk@usrbin.de>
2012-09-06 23:47:04 +0200

    updated console application creation, fixes composer setup for Yii 1.1.12

--- install/P3Setup.php
+++ install/P3Setup.php
@@ -4,34 +4,39 @@ namespace install;
 
 use Composer\Script\Event;
 
-class P3Setup {
+class P3Setup
+{
 
-    public static function preInstall(Event $event) {
+    public static function preInstall(Event $event)
+    {
         $composer = $event->getComposer();
         // do stuff
         echo "Welcome to Phundament Installation 3 via composer\n\n";
-        echo "This setup script will download all packages specified in composer.json. It will also trigger the creation of an ".
-            "Yii web application, please answer the upcoming confirmation questions with [y]es.\n\n";
-        if(self::confirm("Install Phundament 3 now?")) {
+        echo "This setup script will download all packages specified in composer.json. It will also trigger the creation of an " .
+        "Yii web application, please answer the upcoming confirmation questions with [y]es.\n\n";
+        if (self::confirm("Install Phundament 3 now?")) {
 
         } else {
             exit("Installation aborted.\n");
         }
     }
 
-    public static function postInstall(Event $event) {
+    public static function postInstall(Event $event)
+    {
         $composer = $event->getComposer();
         // do stuff
         echo "\n\nInstallation completed.\n\nThank you for choosing Phundament 3!\n\n";
     }
 
-    public static function preUpdate(Event $event) {
+    public static function preUpdate(Event $event)
+    {
         $composer = $event->getComposer();
         // do stuff
         echo "Welcome to Phundament Installation 3 via composer\n\nUpdating your application to the lastest available packages...\n";
     }
 
-    public static function postUpdate(Event $event) {
+    public static function postUpdate(Event $event)
+    {
         $composer = $event->getComposer();
         // do stuff
         echo "\n\nUpdate completed.\n\n";
@@ -43,12 +48,16 @@ class P3Setup {
       // do stuff
       } */
 
-    public static function postPackageInstall(Event $event) {
+    public static function postPackageInstall(Event $event)
+    {
         $installedPackage = $event->getOperation()->getPackage();
+
+        $app = self::getYiiApplication();
+        $commandPath = \Yii::getFrameworkPath() . DIRECTORY_SEPARATOR . 'cli' . DIRECTORY_SEPARATOR . 'commands';
+
         // do stuff
         switch ($installedPackage->getName()) {
             case "yiisoft/yii":
-                require_once(dirname(__FILE__) . '/../protected/extensions/yiisoft/yii/framework/yii.php');
                 @mkdir(dirname(__FILE__) . '/../runtime', 0777);
                 @chmod(dirname(__FILE__) . '/../runtime', 0777);
 
@@ -72,15 +81,13 @@ class P3Setup {
                 return;
                 break;
             case "mishamx/yii-user":
-                require_once(dirname(__FILE__) . '/../protected/yiic.php');
                 $args = array('yiic', 'migrate', '--migrationPath=ext.phundament.p3admin.modules-install.user.migrations', '--migrationTable=migration_module_user', '--interactive=0');
                 break;
             case "crisu83/yii-rights":
-                require_once(dirname(__FILE__) . '/../protected/yiic.php');
+                #exit();
                 $args = array('yiic', 'migrate', '--migrationPath=ext.phundament.p3admin.modules-install.rights.migrations', '--migrationTable=migration_module_rights', '--interactive=0');
                 break;
             case "phundament/p3media":
-                require(dirname(__FILE__) . '/../protected/yiic.php');
                 @mkdir(dirname(__FILE__) . '/../protected/data/p3media', 0777);
                 @mkdir(dirname(__FILE__) . '/../protected/data/p3media-import', 0777);
                 @chmod(dirname(__FILE__) . '/../protected/data/p3media', 0777);
@@ -88,36 +95,35 @@ class P3Setup {
                 $args = array('yiic', 'migrate', '--migrationPath=ext.phundament.p3media.migrations', '--migrationTable=migration_module_p3media', '--interactive=0');
                 break;
             case "phundament/p3widgets":
-                require_once(dirname(__FILE__) . '/../protected/yiic.php');
                 $args = array('yiic', 'migrate', '--migrationPath=ext.phundament.p3widgets.migrations', '--migrationTable=migration_module_p3widgets', '--interactive=0');
                 break;
             case "phundament/p3pages":
-                require_once(dirname(__FILE__) . '/../protected/yiic.php');
                 $args = array('yiic', 'migrate', '--migrationPath=ext.phundament.p3pages.migrations', '--migrationTable=migration_module_p3pages', '--interactive=0');
                 break;
             case "phundament/themes/p3bootstrap":
-                require_once(dirname(__FILE__) . '/../protected/yiic.php');
                 $args = array('yiic', 'composerPackage');
-                $runner = new \CConsoleCommandRunner();
                 $commandPath = \Yii::app()->basePath . DIRECTORY_SEPARATOR . 'extensions' . DIRECTORY_SEPARATOR . $installedPackage->getName() . DIRECTORY_SEPARATOR . "commands";
-                $runner->addCommands($commandPath);
-                $runner->run($args);
-                return;
                 break;
             default:
                 return;
         }
-        $runner = new \CConsoleCommandRunner();
-        $commandPath = \Yii::getFrameworkPath() . DIRECTORY_SEPARATOR . 'cli' . DIRECTORY_SEPARATOR . 'commands';
-        $runner->addCommands($commandPath);
 
-        $runner->run($args);
+        $app->commandRunner->addCommands(YII_PATH.'/cli/commands');
+        $app->commandRunner->addCommands($commandPath);
+        $app->commandRunner->run($args);
     }
 
-    public static function postPackageUpdate(Event $event) {
+    public static function postPackageUpdate(Event $event)
+    {
         $installedPackage = $event->getOperation()->getTargetPackage();
-        require_once(dirname(__FILE__) . '/../protected/yiic.php');
+        
+        $app = self::getYiiApplication();
+        $commandPath = \Yii::getFrameworkPath() . DIRECTORY_SEPARATOR . 'cli' . DIRECTORY_SEPARATOR . 'commands';
+
         switch ($installedPackage->getName()) {
+            case "yiisoft/yii":
+                return;
+                break;
             case "mishamx/yii-user":
                 $args = array('yiic', 'migrate', '--migrationPath=ext.phundament.p3admin.modules-install.user.migrations', '--migrationTable=migration_module_user', '--interactive=0');
                 break;
@@ -134,37 +140,46 @@ class P3Setup {
                 $args = array('yiic', 'migrate', '--migrationPath=ext.phundament.p3pages.migrations', '--migrationTable=migration_module_p3pages', '--interactive=0');
                 break;
             case "phundament/themes/p3bootstrap":
-                echo "NOTE! Run $ yiic p3bootstrap-compser command manually, if you want to update theme files";
-                /*$args = array('yiic', 'composerPackage');
-                $runner = new \CConsoleCommandRunner();
+                $args = array('yiic', 'composerPackage');
                 $commandPath = \Yii::app()->basePath . DIRECTORY_SEPARATOR . 'extensions' . DIRECTORY_SEPARATOR . $installedPackage->getName() . DIRECTORY_SEPARATOR . "commands";
-                $runner->addCommands($commandPath);
-                $runner->run($args);*/
-                return;
                 break;
             default:
                 return;
         }
-        $runner = new \CConsoleCommandRunner();
-        $commandPath = \Yii::getFrameworkPath() . DIRECTORY_SEPARATOR . 'cli' . DIRECTORY_SEPARATOR . 'commands';
-        $runner->addCommands($commandPath);
 
-        $runner->run($args);
+        $app->runner->addCommands($commandPath);
+        $app->runner->run($args);
     }
 
-	/**
-	 * Asks user to confirm by typing y or n.
+    /**
+     * Asks user to confirm by typing y or n.
      *
      * Credits to Yii CConsoleCommand
-	 *
-	 * @param string $message to echo out before waiting for user input
-	 * @return bool if user confirmed
-	 *
-	 * @since 0.5
-	 */
-	public static function confirm($message)
-	{
-		echo $message.' [yes|no] ';
-		return !strncasecmp(trim(fgets(STDIN)),'y',1);
-	}
+     *
+     * @param string $message to echo out before waiting for user input
+     * @return bool if user confirmed
+     *
+     * @since 0.5
+     */
+    public static function confirm($message)
+    {
+        echo $message . ' [yes|no] ';
+        return !strncasecmp(trim(fgets(STDIN)), 'y', 1);
+    }
+
+
+
+    private static function getYiiApplication(){
+        require_once(dirname(__FILE__) . '/../protected/extensions/yiisoft/yii/framework/yii.php');
+        spl_autoload_register(array('YiiBase', 'autoload'));
+
+        if (\Yii::app() === null) {
+            $config = dirname(__FILE__) . '/../protected/config/console.p3.php';
+            $app=\Yii::createConsoleApplication($config);
+        } else {
+            $app=\Yii::app();
+        }
+        return $app;
+    }
+
 }
