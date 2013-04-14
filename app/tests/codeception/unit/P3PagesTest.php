6450b15797608468fb2eea433ecb00f5a1614d20
Tobias Munk <schmunk@usrbin.de>
2013-04-14 20:36:44 +0200

    updated unit test

--- app/tests/codeception/unit/P3PagesTest.php
+++ app/tests/codeception/unit/P3PagesTest.php
@@ -22,21 +22,21 @@ class P3PagesTest extends \Codeception\TestCase\Test
         Yii::import('p3pages.*');
 
         $model = new P3Page();
-        $model->route = '{route:""site/index}';
+        $model->route = '{"route":"site/index"}';
         $model->save();
 
-        #$this->codeGuy->seeInDatabase($model->tableSchema->name,array('id' => '1'));
-        #$this->codeGuy->seeInDatabase('p3_page_meta',array('id' => '1'));
+        $this->codeGuy->seeInDatabase('p3_page',array('id' => '1'));
+        $this->codeGuy->seeInDatabase('p3_page_meta',array('id' => '1'));
     }
 
     public function testDeletePage()
     {
         Yii::import('p3pages.*');
 
-        #$model = P3Page::model()->findByPk(1);
-        #$model->delete();
+        $model = P3Page::model()->findByPk(1);
+        $model->delete();
 
-        #$this->codeGuy->dontSeeInDatabase($model->tableSchema->name,array('id' => '1'));
-        #$this->codeGuy->dontSeeInDatabase('p3_page_meta',array('id' => '1'));
+        $this->codeGuy->dontSeeInDatabase($model->tableSchema->name,array('id' => '1'));
+        $this->codeGuy->dontSeeInDatabase('p3_page_meta',array('id' => '1'));
     }
 }
