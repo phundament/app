fc5abcd9ef0b47ac16b52cbd6b2e452643e36e7a
Tobias Munk <schmunk@usrbin.de>
2013-04-15 23:39:36 +0200

    cleanup

--- test.sh
+++ test.sh
@@ -1,14 +1,11 @@
 rm app/data/test.db
-app/yiic migrate --connectionID=dbTest --interactive=0
-chmod 777 app/data/test.db
+app/yiic migrate --connectionID=dbTest --interactive=0 > /dev/null
 vendor/bin/codecept run unit
 
 rm app/data/test.db
-app/yiic migrate --connectionID=dbTest --interactive=0
-chmod 777 app/data/test.db
+app/yiic migrate --connectionID=dbTest --interactive=0 > /dev/null
 vendor/bin/codecept run functional
 
 rm app/data/test.db
-app/yiic migrate --connectionID=dbTest --interactive=0
-chmod 777 app/data/test.db
+app/yiic migrate --connectionID=dbTest --interactive=0 > /dev/null
 vendor/bin/codecept run acceptance
