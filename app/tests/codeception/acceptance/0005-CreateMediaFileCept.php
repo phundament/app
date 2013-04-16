8d0f9c001cdd772f8e58072a4aa0dd87ddca2377
Mario Walz <m.walz@herzogkommunikation.de>
2013-04-16 10:29:08 +0200

    added tests

---
+++ app/tests/codeception/acceptance/0005-CreateMediaFileCept.php
@@ -0,0 +1,13 @@
+<?php
+$I = new WebGuy($scenario);
+$I->wantTo('create a media file');
+
+Codeception\Module\WebHelper::login($I, 'editor', 'editor');
+
+$I->amOnPage('?r=p3media&lang=en');
+
+$I->click(' Create File');
+$I->see('Create P3 Media','h1');
+$I->fillField('P3Media[title]','file');
+$I->click('Save');
+$I->see('file','h5');
