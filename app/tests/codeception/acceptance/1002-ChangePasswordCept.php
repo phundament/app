845a88511ab64684096477093dd1550013c607f5
Mario Walz <m.walz@herzogkommunikation.de>
2013-04-25 11:46:39 +0200

    updated tests

--- app/tests/codeception/acceptance/1002-ChangePasswordCept.php
+++ app/tests/codeception/acceptance/1002-ChangePasswordCept.php
@@ -9,8 +9,8 @@ Codeception\Module\WebHelper::login($I, 'editor', 'editor');
 // Change Password
 
 $I->amOnPage('?r=user/profile&lang=en');
-$I->click('Change password');
-$I->see('Change password');
+$I->click('Change password','.nav li a');
+$I->see('Change password','h1');
 $I->fillField('UserChangePassword[oldPassword]', 'editor');
 $I->fillField('UserChangePassword[password]','editor');
 $I->fillField('UserChangePassword[verifyPassword]','editor');
