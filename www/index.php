b20c99fdcbc70b8047eb309701ff275b8a215cc0
Tobias Munk <schmunk@usrbin.de>
2013-04-26 08:57:02 +0200

    added composer autoloading support

--- www/index.php
+++ www/index.php
@@ -9,5 +9,11 @@ defined('YII_DEBUG') or define('YII_DEBUG',true);
 // specify how many levels of call stack should be shown in each log message
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 
+// register composer autoloader
+require_once(dirname(__FILE__).'/../vendor/autoload.php');
+
+// load Yii
 require_once($yii);
+
+// start web application
 Yii::createWebApplication($config)->run();
