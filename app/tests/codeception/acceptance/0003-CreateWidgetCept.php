defdfbe775633e8bc635c8399966a6ac247985a0
Mario Walz <m.walz@herzogkommunikation.de>
2013-05-21 15:53:13 +0200

    updated tests

--- app/tests/codeception/acceptance/0003-CreateWidgetCept.php
+++ app/tests/codeception/acceptance/0003-CreateWidgetCept.php
@@ -6,12 +6,12 @@ Codeception\Module\WebHelper::login($I, 'editor', 'editor');
 
 $I->amOnPage('?r=p3widgets/p3Widget/admin&lang=en');
 
-$I->click('Create');
+$I->click(' Create','.btn');
+$I->see('Create','h1');
 $I->selectOption('P3Widget[alias]','Basic HTML Widget');
 $I->fillField('P3Widget[controllerId]','site');
 $I->fillField('P3Widget[containerId]','top');
 $I->fillField('P3Widget[actionName]','index');
-$I->see('P3 Widget Create');
 $I->click('Save');
-$I->see('P3 Widget');
-$I->see('Relations');
+$I->see('Widgets','h1');
+$I->see('Data','h2');
