bda4abda7bb44f13cf02e0912c8a958ce2ea32f2
Tobias Munk <schmunk@usrbin.de>
2013-07-01 11:48:27 +0200

    updated create widget translation test

--- app/tests/codeception/acceptance/0003-CreateWidgetTranslationCept.php
+++ app/tests/codeception/acceptance/0003-CreateWidgetTranslationCept.php
@@ -6,14 +6,8 @@ Codeception\Module\WebHelper::login($I, 'editor', 'editor');
 
 $I->amOnPage('?r=site/index&lang=en');
 $I->See('Translation for','.alert');
-
-$I->click(' Manage','.dropdown-menu li a');
+$I->click("//div[@id='widget-1']//a[contains(@href,'p3widgets/p3WidgetTranslation/create')]");
 $I->see('Widgets','h1');
-$I->click('Widget Translations','a');
-$I->see('Translations','h1');
-$I->click(' Create','.btn');
-$I->fillField('P3WidgetTranslation[language]','en');
+#$I->fillField('P3WidgetTranslation_content','Hello World!');
 $I->click('Save');
-
-$I->amOnPage('?r=site/index&lang=en');
 $I->dontSee('Translation for','.alert');
