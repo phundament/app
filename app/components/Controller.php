da5ef79e8943ff4936120ddb30adbcfc6eb1ac9d
Tobias Munk <schmunk@usrbin.de>
2013-07-12 19:47:50 +0200

    fixed homeUrl

--- app/components/Controller.php
+++ app/components/Controller.php
@@ -20,4 +20,9 @@ class Controller extends CController
 	 * for more details on how to specify this property.
 	 */
 	public $breadcrumbs=array();
+
+    function init(){
+        parent::init();
+        Yii::app()->homeUrl = $this->createUrl('/');
+    }
 }
