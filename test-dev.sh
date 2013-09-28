7c6b23fe8fe41251aeff9f64995a967d6cc6356a
Tobias Munk <schmunk@usrbin.de>
2013-09-28 03:45:30 +0200

    updated test script, kill php process

--- test-dev.sh
+++ test-dev.sh
@@ -1,4 +1,4 @@
-php54 -S localhost:31415 -t www/ &
+php54 -S localhost:31415 -t www/ > /dev/null 2>&1 & echo $! > test-php.pid
 
 rm app/data/test.db
 app/yiic migrate --connectionID=dbTest --interactive=0 > /dev/null
@@ -11,3 +11,6 @@ codecept.phar run functional
 rm app/data/test.db
 app/yiic migrate --connectionID=dbTest --interactive=0 > /dev/null
 codecept.phar run acceptance
+
+kill -9 `cat test-php.pid`
+rm test-php.pid
