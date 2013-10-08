bdab45db838ca3a56443e2ef83815c48e35a00fe
Tobias Munk <schmunk@usrbin.de>
2013-10-08 12:59:50 +0200

    hardened tests

--- tests/codeception/acceptance/0005-CreateMediaFileCept.php
+++ tests/codeception/acceptance/0005-CreateMediaFileCept.php
@@ -10,5 +10,5 @@ $I->click(' Create File');
 $I->see('Media Create','h1');
 $I->fillField('P3Media[default_title]','file-12345');
 $I->attachFile('fileUpload','phundament.png');
-$I->click('Save');
+$I->click('Save','FORM');
 $I->see('file-12345','h5');
--- tests/codeception/acceptance/0005-CreateMediaFolderCept.php
+++ tests/codeception/acceptance/0005-CreateMediaFolderCept.php
@@ -9,5 +9,5 @@ $I->amOnPage('?r=p3media&lang=en');
 $I->click(' Create Folder');
 $I->see('Media Create');
 $I->fillField('P3Media[default_title]','folder');
-$I->click('Save');
+$I->click('Save','FORM');
 $I->see('folder','div ul li');
