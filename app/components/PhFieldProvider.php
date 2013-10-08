e39d8aedf437ce3f56ec1ddd2bc2f0c3b08fcf67
Tobias Munk <schmunk@usrbin.de>
2013-10-08 15:01:54 +0200

    updated Phundament field provider

--- app/components/PhFieldProvider.php
+++ app/components/PhFieldProvider.php
@@ -48,7 +48,7 @@ class PhFieldProvider extends GtcCodeProvider
                 break;
             case 'info_php_json':
             case 'info_image_magick_json':
-                return "echo \$model->{$column->name}";
+                return "echo CVarDumper::dumpAsString(CJSON::decode(\$model->{$column->name}), 5, true)";
                 break;
 
         }
@@ -62,4 +62,97 @@ class PhFieldProvider extends GtcCodeProvider
             );";
         }
     }
+
+    public function generateAttribute($modelClass, $column, $view = false)
+    {
+        #var_dump($modelClass, $column);exit;
+        switch ($column->name) {
+            // disabled fields
+            case 'copied_from_id':
+            case 'created_at':
+            case 'updated_at':
+            case 'access_owner':
+            case 'original_name':
+            case 'path':
+            case 'hash':
+            case 'mime_type':
+            case 'size':
+                return "array(
+                        'name' => '{$column->name}',
+                        'type' => 'raw',
+                        'value' => \$model->{$column->name}
+                    ),\n";
+                break;
+            // media select widget
+            case 'default_p3media_id':
+            case 'p3media_id':
+                #return "\$this->widget('P3MediaSelect', array('model'=>\$model,'attribute'=>'{$column->name}'))";
+                break;
+            case 'info_php_json':
+            case 'info_image_magick_json':
+                return "array(
+                        'name' => 'Image',
+                        'type' => 'raw',
+                        'value' => CVarDumper::dumpAsString(CJSON::decode(\$model->{$column->name}), 5, true)
+                    ),\n";
+
+                break;
+
+        }
+
+        if ($column->isForeignKey) {
+            return null;
+        } elseif ($modelClass == 'vendor.phundament.p3Media.models.P3Media' && $column->name == 'id') {
+            $code = "array(
+                        'name' => 'Image',
+                        'type' => 'raw',
+                        'value' => \$model->image('p3media-manager')
+                    ),\n";
+        } elseif (strtoupper($column->dbType) == 'TEXT') {
+            $code = "array(
+                        'name' => '{$column->name}',
+                        'type' => 'raw',
+                        'value' => \$model->{$column->name}
+                    ),\n";
+        } else {
+            $code = null;
+        }
+        return $code;
+    }
+
+    public function generateHtml($modelClass, $column, $view = false)
+    {
+
+        switch ($view) {
+            case 'prepend':
+                switch ($column->name) {
+                    case 'tree_parent_id':
+                        return "echo '<h3>Tree</h3>'";
+                        break;
+                    case 'default_page_title':
+                        return "echo '<h3>A) Title, Layout & View</h3>'";
+                        break;
+                    case 'url_json':
+                        return "echo '<h3>B) Weiterleitung</h3>'";
+                        break;
+                    case 'access_owner':
+                        return "echo '<h3>Access</h3>'";
+                        break;
+                    case 'default_url_param':
+                        return "echo '<h3>SEO</h3>'";
+                        #return "echo '{$modelClass}{$column->name}{$view}'";
+                        break;
+                    case 'original_name':
+                        return "echo '<h3>File</h3>'";
+                        #return "echo '{$modelClass}{$column->name}{$view}'";
+                        break;
+                    case 'container_id':
+                        return "echo '<h3>Position</h3>'";
+                        #return "echo '{$modelClass}{$column->name}{$view}'";
+                        break;
+                }
+                break;
+        }
+    }
+
 }
