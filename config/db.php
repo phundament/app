50759383ad3662075e027405c3bfcf69ad511aba
Tobias Munk <schmunk@usrbin.de>
2014-02-04 13:27:17 +0100

    switched to SQLite as default database

--- config/db.php
+++ config/db.php
@@ -1,9 +1,17 @@
 <?php
 
+
 return [
     'class'    => 'yii\db\Connection',
-    'dsn'      => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn'      => 'sqlite:'.__DIR__.'/../data/app.db',
+];
+
+/*
+return [
+    'class'    => 'yii\db\Connection',
+    'dsn'      => 'mysql:host=localhost;dbname=p4',
+    'username' => 'test',
+    'password' => 'test',
     'charset'  => 'utf8',
 ];
+*/
