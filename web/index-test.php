220b22b3f54480329d238cd17be6480495bc888d
Tobias Munk <schmunk@usrbin.de>
2014-02-18 16:16:07 +0100

    adjusted test configuration

--- web/index-test.php
+++ web/index-test.php
@@ -1,7 +1,7 @@
 <?php
 
 // NOTE: Make sure this file is not accessible when deployed to production
-if (!in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1'])) {
+if (!in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1', '192.168.33.101'])) {
     die('You are not allowed to access this file.');
 }
 
