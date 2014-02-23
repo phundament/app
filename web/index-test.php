d354fefc1f6376ff60ce3cbb60100260c66216b9
Tobias Munk <schmunk@usrbin.de>
2014-02-23 23:11:47 +0100

    switched to yii2-dev dependency, added framework switch to index.php

--- web/index-test.php
+++ web/index-test.php
@@ -8,8 +8,15 @@ if (!in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1', '192.168.33.101']))
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'test');
 
+// add composer autoloader
 require(__DIR__ . '/../vendor/autoload.php');
-require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
+
+// prefer yii2-dev if it exists
+if (is_file(__DIR__ . '/../vendor/yiisoft/yii2-dev/framework/Yii.php')) {
+    require(__DIR__ . '/../vendor/yiisoft/yii2-dev/framework/Yii.php');
+} else {
+    require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
+}
 
 $config = require(__DIR__ . '/../tests/acceptance/_config.php');
 
