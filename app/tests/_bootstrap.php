dd0194f8210eb6bcc955b629f02d494f215559d6
Tobias Munk <schmunk@usrbin.de>
2014-03-04 17:20:17 +0100

    fixed paths

--- app/tests/_bootstrap.php
+++ app/tests/_bootstrap.php
@@ -5,15 +5,15 @@
 defined('TEST_ENTRY_URL') or define('TEST_ENTRY_URL', '/index-test.php');
 
 // the entry script file path for functional and acceptance tests
-defined('TEST_ENTRY_FILE') or define('TEST_ENTRY_FILE', dirname(__DIR__) . '/web/index-test.php');
+defined('TEST_ENTRY_FILE') or define('TEST_ENTRY_FILE', dirname(__DIR__.'..') . '/web/index-test.php');
 
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 
 defined('YII_ENV') or define('YII_ENV', 'test');
 
-require_once(__DIR__ . '/../vendor/autoload.php');
+require_once(__DIR__ . '/../../vendor/autoload.php');
 
-require_once(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
+require_once(__DIR__ . '/../../vendor/yiisoft/yii2/Yii.php');
 
 // set correct script paths
 $_SERVER['SCRIPT_FILENAME'] = TEST_ENTRY_FILE;
