0d673ae4eb2a0eb36481632f58fff44d52529e91
Tobias Munk <schmunk@usrbin.de>
2014-03-21 19:01:41 +0100

    reverted user login to app-basic-default

--- app/views/layouts/main.php
+++ app/views/layouts/main.php
@@ -42,7 +42,7 @@ AppAsset::register($this);
                     ['label' => 'About', 'url' => ['/site/about']],
                     ['label' => 'Contact', 'url' => ['/site/contact']],
                     Yii::$app->user->isGuest ?
-                        ['label' => 'Login', 'url' => ['/usr/login']] :
+                        ['label' => 'Login', 'url' => ['/site/login']] :
                         [
                             'label'       => 'Logout (' . Yii::$app->user->identity->username . ')',
                             'url'         => ['/site/logout'],
