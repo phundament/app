87d2808d373f7073786d9a40fa352d5ccbac59cf
Tobias Munk <schmunk@usrbin.de>
2014-05-10 00:43:21 +0200

    updated name and navbar style

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -25,7 +25,7 @@ AppAsset::register($this);
     <div class="wrap">
         <?php
             NavBar::begin([
-                'brandLabel' => 'My Company',
+                'brandLabel' => 'Company Inc.',
                 'brandUrl' => Yii::$app->homeUrl,
                 'options' => [
                     'class' => 'navbar-inverse navbar-fixed-top',
--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -26,10 +26,10 @@ AppAsset::register($this);
     <div class="wrap">
         <?php
             NavBar::begin([
-                'brandLabel' => 'My Company',
+                'brandLabel' => 'Company Inc.',
                 'brandUrl' => Yii::$app->homeUrl,
                 'options' => [
-                    'class' => 'navbar-inverse navbar-fixed-top',
+                    'class' => 'navbar navbar-fixed-top',
                 ],
             ]);
             $menuItems = [
