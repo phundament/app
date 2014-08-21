7bc042fec9ce5c23e5fc457a332fa61ef6d07df3
Tobias Munk <schmunk@usrbin.de>
2014-08-21 23:46:42 +0200

    updated index view (links & text)

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -5,45 +5,45 @@ $this->title = 'My Yii Application';
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Congratulations!</h1>
+        <h1>Scho!</h1>
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
+        <p class="lead">You have successfully created your Phundament application template.</p>
 
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+        <p><a class="btn btn-lg btn-success" href="https://github.com/phundament/app/blob/4.0/docs/index.md" target="_blank">Get started</a></p>
     </div>
 
     <div class="body-content">
 
         <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
+            <div class="col-sm-4">
+                <h2>Docs</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <p>Phundament and Yii2 online guides and class references.</p>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+                <div class="btn-group-vertical">
+                    <a class="btn btn-default" href="https://github.com/phundament/app/tree/4.0#documentation-overview">Phundament Documentation &raquo;</a>
+                    <a class="btn btn-default" href="http://www.yiiframework.com/doc-2.0/guide-index.html">Yii Documentation &raquo;</a>
+                </div>
             </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
+            <div class="col-sm-4">
+                <h2>Extensions</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <p>Install additional components or modules to your application.</p>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
+                <div class="btn-group-vertical">
+                    <a class="btn btn-default" href="https://packagist.org/search/?q=yii2">Extensions on Packagist &raquo;</a>
+                    <a class="btn btn-default" href="https://github.com/phundament/yii2-extension-requests">Extension Requests &raquo;</a>
+                </div>
             </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
+            <div class="col-sm-4">
+                <h2>Support</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <p>Got stuck? Get help or report an issue.</p>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+                <div class="btn-group-vertical">
+                    <a class="btn btn-default" href="https://github.com/phundament/app/issues?q=is%3Aopen+is%3Aissue+milestone%3A4.0">GitHub Issues &raquo;</a>
+                    <a class="btn btn-default" href="http://stackoverflow.com/questions/tagged/phundament">Stackoverflow Q&A &raquo;</a>
+                </div>
             </div>
         </div>
 
