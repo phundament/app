c5d5de90b93ea8b43a0a241c1c69b08c771735fd
Tobias Munk <schmunk@usrbin.de>
2014-08-21 23:45:08 +0200

    removed gii module from frontend YII_ENV_TEST config

--- init-env/dev/frontend/config/main-local.php
+++ init-env/dev/frontend/config/main-local.php
@@ -6,9 +6,6 @@ if (!YII_ENV_TEST) {
     // configuration adjustments for 'dev' environment
     $config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = 'yii\debug\Module';
-
-    $config['bootstrap'][] = 'gii';
-    $config['modules']['gii'] = 'yii\gii\Module';
 }
 
 return $config;
