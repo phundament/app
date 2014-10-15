539d9eccb519f0eb461f749da1ac7933fd2082f0
Tobias Munk <schmunk@usrbin.de>
2014-10-15 12:20:12 +0200

    fixed typo

--- environments/dotenv/common/config/env.php
+++ environments/dotenv/common/config/env.php
@@ -5,6 +5,6 @@ use \Dotenv;
 Dotenv::load(__DIR__.'/../..');
 
 Dotenv::required('YII_DEBUG',["","0","1","true"]);
-Dotenv::required('YII_ENV',['dev','env','prod']);
+Dotenv::required('YII_ENV',['dev','prod','test']);
 Dotenv::required(['APP_NAME','SUPPORT_EMAIL','ADMIN_EMAIL']);
 Dotenv::required(['DATABASE_DSN','DATABASE_USER','DATABASE_PASSWORD']);
