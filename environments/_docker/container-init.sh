c34bdd2cda84f7e5f3baf6fe12cf25c2f54aeb05
Tobias Munk <schmunk@usrbin.de>
2014-11-10 22:01:02 +0100

    fixed typo

--- environments/_docker/container-init.sh
+++ environments/_docker/container-init.sh
@@ -1,5 +1,5 @@
 cd /app
 ./yii migrate/up --interactive=0
-./yii user/create $ADMIN_EMAIL admin
-./yii user/password admin $ADMIN_PASSWORD
+./yii user/create $APP_ADMIN_EMAIL admin
+./yii user/password admin $APP_ADMIN_PASSWORD
 ./yii user/confirm admin
