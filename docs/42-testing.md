e40eabb1cfba5eda26c04657b222a987463a5f9e
Tobias Munk <schmunk@usrbin.de>
2015-01-03 22:07:12 +0100

    test setup cleanup

--- docs/42-testing.md
+++ docs/42-testing.md
@@ -1,6 +1,28 @@
 Testing
 ===========
 
+### Running codeception in isolated Docker containers
+
+Bring up the test container and execute the codeception test suites with docker...
+
+```
+fig up testweb
+docker exec app_testweb_1 codecept run
+```
+
+The following commands can be used to determine the container name to use with docker...
+
+```
+docker ps
+```
+
+Or to rebuild the codeception classes...
+
+```
+docker exec app_testweb_1 codecept build
+```
+
+
 Install required packages and build test classes
 
 ```
--- tests/codeception/config/config.php
+++ tests/codeception/config/config.php
@@ -4,25 +4,11 @@
  */
 return [
     'components' => [
-        /*'db'           => [
-            'class'       => 'yii\db\Connection',
-            'dsn'         => getenv('DATABASE_DSN'),
-            'username'    => getenv('DATABASE_USER'),
-            'password'    => getenv('DATABASE_PASSWORD'),
-            'charset'     => 'utf8',
-            'tablePrefix' => getenv('DATABASE_TABLE_PREFIX'),
-        ],*/
         'mailer' => [
             'useFileTransport' => true,
         ],
-        'urlManager' => [
-            #'showScriptName' => true,
-            #'enablePrettyUrl' => false,
-        ],
         'request' => [
-            #'class' => 'yii\web\Request',
-            #'baseUrl' => 'http://localhost',
            'cookieValidationKey' => getenv('APP_COOKIE_VALIDATION_KEY')
-        ]
+        ],
     ],
 ];
