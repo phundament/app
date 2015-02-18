1450711ca47d08e7054ee0be7f665ac4b47072ad
Tobias Munk <schmunk@usrbin.de>
2015-02-18 05:24:52 +0100

    removed docs test

--- tests/codeception/acceptance/DocsCept.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$I = new AcceptanceTester($scenario);
-$I->wantTo('ensure that docs pages work');
-$I->amOnPage(Yii::$app->urlManager->createUrl(['/docs']));
-$I->see('Phundament Guide');
-
-$I->amOnPage(Yii::$app->urlManager->createUrl(['/docs', 'file' => '20-installation.md']));
-$I->see('Installation','h1');
