fa2e260e77f850036f20e92c7751be9363b75c11
Tobias Munk <schmunk@usrbin.de>
2015-03-03 23:18:20 +0100

    updated assets, ignored asset-prod output folder, files are only added to :production container

---
+++ config/assets.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Configuration file for the "yii asset" console command.
+ * Note that in the console environment, some path aliases like '@webroot' and '@web' may not exist.
+ * Please define these missing path aliases.
+ */
+return [
+    // Adjust command/callback for JavaScript files compressing:
+    'jsCompressor'  => 'java -jar /root/compiler.jar --js {from} --js_output_file {to}',
+    // Adjust command/callback for CSS files compressing:
+    'cssCompressor' => 'yui-compressor --type css {from} -o {to}',
+    // The list of asset bundles to compress:
+    'bundles'       => [
+        'app\assets\AppAsset',
+        'yii\bootstrap\BootstrapAsset',
+        'app\modules\admin\assets\AdminAsset',
+        \dmstr\web\AdminLteAsset::className()
+    ],
+    // Asset bundle for compression output:
+    'targets'       => [
+        'frontend' => [
+            'class'    => 'yii\web\AssetBundle',
+            'basePath' => '@app/web/assets-prod',
+            'baseUrl'  => '@web/assets-prod',
+            'js'       => 'js/frontend-{hash}.js',
+            'css'      => 'css/frontend-{hash}.css',
+            'depends'  => [
+                // Include only 'frontend' assets:
+                'app\assets\AppAsset',
+            ],
+        ],
+        'backend' => [
+            'class'    => 'yii\web\AssetBundle',
+            'basePath' => '@app/web/assets-prod',
+            'baseUrl'  => '@web/assets-prod',
+            'js'       => 'js/backend-{hash}.js',
+            'css'      => 'css/backend-{hash}.css',
+            'depends'  => [
+                // Include only 'backend' assets:
+                'app\modules\admin\assets\AdminAsset',
+                \dmstr\web\AdminLteAsset::className()
+            ],
+        ],
+        'all'          => [
+            'class'    => 'yii\web\AssetBundle',
+            'basePath' => '@app/web/assets-prod',
+            'baseUrl'  => '@web/assets-prod',
+            'js'       => 'js/all-{hash}.js',
+            'css'      => 'css/all-{hash}.css',
+        ],
+    ],
+    // Asset manager configuration:
+    'assetManager'  => [
+        'basePath' => '/app/web/assets-prod',
+        'baseUrl'  => '/assets-prod'
+    ],
+];
