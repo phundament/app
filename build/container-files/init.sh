b80da77ea0889633b5a0183e454fa448fe3a72e5
Tobias Munk <schmunk@usrbin.de>
2015-06-17 04:36:33 +0200

    added permission workaround

--- build/container-files/init.sh
+++ build/container-files/init.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
 mkdir /app/web/assets /app/runtime
-chmod 777 /app/web/assets /app/runtime
+chmod -R 777 /app/web/assets /app/runtime
 
 composer install
 
 ./yii app/create-mysql-db
 ./yii migrate --interactive=0
 ./yii app/setup-admin-user --interactive=0
+
+# TODO: temporary workaround, folders create by appcli, are not writable by appfpm
+chmod -R 777 /app/web/assets /app/runtime
 
 echo "\nApplication initialized"
 
