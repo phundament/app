7a9d24a76eb92f38b48501ed1feb2a8523cec3ad
Tobias Munk <schmunk@usrbin.de>
2015-12-09 18:44:16 +0100

    added env variable for migration lookups to init script

--- src/init.sh
+++ src/init.sh
@@ -4,7 +4,7 @@ mkdir -p /app/web/assets /app/runtime
 mkdir -p /app/runtime/mail /app/runtime/cache /app/runtime/debug
 
 ./yii app/create-mysql-db
-./yii migrate --interactive=0
+./yii migrate --interactive=0 --migrationLookup=${APP_MIGRATION_LOOKUP}
 ./yii app/setup-admin-user --interactive=0
 
 # TODO: temporary workaround, folders create by appcli, are not writable by appfpm
