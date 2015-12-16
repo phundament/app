f617a2a052c341527e4adb86817936e2aeb65d7b
Tobias Munk <schmunk@usrbin.de>
2015-12-16 22:57:02 +0100

    added dev tests (allow_failure: true)

--- build/ci/test.sh
+++ build/ci/test.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-# Cleanup, install, setup, build, test, tag
 make TEST setup up
 make TEST run-tests codecept_opts='unit prod --html=_report_unit.html'
+make TEST run-tests codecept_opts='functional prod --html=_report_acceptance.html'
 make TEST run-tests codecept_opts='acceptance prod --html=_report_acceptance.html'
 
 exit 0
