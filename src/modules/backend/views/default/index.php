2fc7c6ad044c7f21afee76755d9d3cb3a6574589
Tobias Munk <schmunk@usrbin.de>
2015-12-16 01:53:50 +0100

    added backend views for admin lte

--- src/modules/backend/views/default/index.php
+++ src/modules/backend/views/default/index.php
@@ -1,8 +1,183 @@
-<?php
+<div class="row">
+    <div class="col-md-3 col-xs-6">
+        <!-- small box -->
+        <div class="small-box bg-blue">
+            <div class="inner">
+                <h3>
+                     <?= YII_ENV ?>
+                </h3>
 
-// Note: Requiring the example-view from the dmstr/yii2-adminlte-asset extension is just meant as quickstart.
-// If you need to customize the backend layout or dashboard, copy the view file content into this location.
+                <p>
+                    Go to Frontend
+                </p>
+            </div>
+            <div class="icon">
+                <i class="ion ion-home"></i>
+            </div>
+            <a href="<?= \yii\helpers\Url::to('site/index') ?>" class="small-box-footer">
+                Homepage <i class="fa fa-arrow-circle-right"></i>
+            </a>
+        </div>
+    </div>
+    <!-- ./col -->
 
-require(Yii::getAlias('@vendor/dmstr/yii2-adminlte-asset/example-views/phundament/app/default/index.php'));
+    <?php if (Yii::$app->user->identity->isAdmin): ?>
 
-?>
+        <div class="col-md-3 col-xs-6">
+            <!-- small box -->
+            <div class="small-box bg-aqua">
+                <div class="inner">
+                    <h3>
+                        <?= \dektrium\user\models\User::find()->count() ?>
+                    </h3>
+
+                    <p>
+                        Users
+                    </p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-person"></i>
+                </div>
+                <a href="<?= \yii\helpers\Url::to(['/user/admin']) ?>" class="small-box-footer">
+                    Manage <i class="fa fa-arrow-circle-right"></i>
+                </a>
+            </div>
+        </div>
+        <!-- ./col -->
+
+        <div class="col-md-3 col-xs-6">
+            <!-- small box -->
+            <div class="small-box bg-orange">
+                <div class="inner">
+                    <h3>
+                        <?= count(\Yii::$app->getModules()) ?>
+                    </h3>
+
+                    <p>
+                        Modules
+                    </p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-stats-bars"></i>
+                </div>
+                <a href="<?= \yii\helpers\Url::to(['/debug']) ?>" class="small-box-footer">
+                    Debug <i class="fa fa-arrow-circle-right"></i>
+                </a>
+            </div>
+
+        </div>
+        <!-- ./col -->
+
+        <div class="col-md-3 col-xs-6">
+            <!-- small box -->
+            <div class="small-box bg-red">
+                <div class="inner">
+                    <h3>
+                        <?= YII_ENV ?>
+                    </h3>
+
+                    <p>
+                        <?= APP_VERSION ?>
+                    </p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-grid"></i>
+                </div>
+                <a href="<?= \yii\helpers\Url::to('http://phundament.com') ?>" target="_blank" class="small-box-footer">
+                    Phundament Online <i class="fa fa-arrow-circle-right"></i>
+                </a>
+            </div>
+        </div>
+        <!-- ./col -->
+    <?php endif; ?>
+
+</div>
+
+<div class="row">
+    <div class="col-sm-12">
+        <!-- Default box -->
+        <div class="box">
+            <div class="box-header">
+                <h3 class="box-title">Languages</h3>
+            </div>
+            <div class="box-body">
+                <?php
+                foreach (\Yii::$app->urlManager->languages AS $name) {
+                    echo yii\helpers\Html::a(
+                        $name,
+                        ['', \Yii::$app->urlManager->languageParam=>$name],
+                        ['class' => 'btn btn-default btn-flat '.(Yii::$app->language == $name?'active':'')]
+                    );
+                }
+                ?>
+            </div>
+            <!-- /.box-body -->
+            <div class="box-footer">
+                <small>Registered in <code>urlManager</code> application component.</small>
+            </div>
+            <!-- /.box-footer-->
+        </div>
+        <!-- /.box -->
+    </div>
+
+</div>
+
+
+<?php if (Yii::$app->user->identity->isAdmin): ?>
+
+    <div class="row">
+        <div class="col-sm-6">
+            <!-- Default box -->
+            <div class="box">
+                <div class="box-header">
+                    <h3 class="box-title">Modules</h3>
+                </div>
+                <div class="box-body">
+                    <?php
+                    foreach (\Yii::$app->getModules() AS $name => $m) {
+                        $module = \Yii::$app->getModule($name);
+                        echo yii\helpers\Html::a(
+                            $module->id,
+                            ['/' . $module->id],
+                            ['class' => 'btn btn-default btn-flat']
+                        );
+                    }
+                    ?>
+                </div>
+                <!-- /.box-body -->
+                <div class="box-footer">
+                    <small>Registered in application from configuration or bootstrapping.</small>
+                </div>
+                <!-- /.box-footer-->
+            </div>
+            <!-- /.box -->
+        </div>
+
+        <div class="col-sm-6">
+            <!-- Default box -->
+            <div class="box">
+                <div class="box-header">
+                    <h3 class="box-title">Documentation</h3>
+                </div>
+                <div class="box-body">
+                    <div class="alert alert-info">
+                        <i class="fa fa-warning"></i>
+                        <b>Notice!</b> Use the <i>yii2-apidoc</i> extension to
+                        create the HTML documentation for this application.
+                    </div>
+                </div>
+                <!-- /.box-body -->
+                <div class="box-footer">
+                    <small>Need help? Get <?= yii\helpers\Html::a(
+                            'support',
+                            'mailto:' . \Yii::$app->params['supportEmail']
+                        ); ?>.
+                    </small>
+                </div>
+                <!-- /.box-footer-->
+            </div>
+            <!-- /.box -->
+        </div>
+    </div>
+
+<?php endif; ?>
--- src/modules/backend/views/layouts/main.php
+++ src/modules/backend/views/layouts/main.php
@@ -1,10 +1,172 @@
 <?php
+use dmstr\widgets\Alert;
+use yii\helpers\Html;
 
-// Note: Requiring the example-view from the dmstr/yii2-adminlte-asset extension is just meant as quickstart.
-// If you need to customize the backend layout or dashboard, copy the view file content into this location.
-
-\app\modules\backend\assets\AdminAsset::register($this);
-
-require(Yii::getAlias('@vendor/dmstr/yii2-adminlte-asset/example-views/phundament/app/layouts/main.php'));
-
+/* @var $this \yii\web\View */
+/* @var $content string */
+$this->title = $this->title . ' [Backend] ' . Yii::$app->params['appName'];
+dmstr\web\AdminLteAsset::register($this);
 ?>
+
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <?= Html::csrfMetaTags() ?>
+    <title><?= Html::encode($this->title) ?></title>
+    <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+    <!-- Ionicons -->
+    <link href="//code.ionicframework.com/ionicons/1.5.2/css/ionicons.min.css" rel="stylesheet" type="text/css"/>
+    <!-- Theme style -->
+    <?php $this->head() ?>
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body class="hold-transition skin-black sidebar-mini">
+<?php $this->beginBody() ?>
+
+<div class="wrapper">
+
+    <header class="main-header">
+        <!-- Logo -->
+        <a href="<?= \Yii::$app->homeUrl ?>" class="logo"><?= getenv('APP_TITLE') ?></a>
+        <!-- Header Navbar: style can be found in header.less -->
+        <nav class="navbar navbar-static-top" role="navigation">
+            <!-- Sidebar toggle button-->
+            <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+                <span class="sr-only">Toggle navigation</span>
+            </a>
+
+            <div class="navbar-custom-menu">
+                <ul class="nav navbar-nav">
+                    <?php if (!\Yii::$app->user->isGuest): ?>
+                        <!-- Messages: style can be found in dropdown.less-->
+                        <li class="dropdown messages-menu">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                <i class="fa fa-envelope-o"></i>
+                                <span class="label label-success">1</span>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li class="header">You have 1 notification(s)</li>
+                                <li>
+                                    <!-- inner menu: contains the actual data -->
+                                    <ul class="menu">
+                                        <li>
+                                            <a href="#">
+                                                <i class="ion ion-ios7-people info"></i> Welcome to Phundament 4!
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="dropdown tasks-menu">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                <i class="fa fa-flag"></i>
+                                <span class="label label-default"><?= count(Yii::$app->urlManager->languages) ?></span>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li class="header">Languages</li>
+                                <li>
+                                    <!-- inner menu: contains the actual data -->
+                                    <ul class="menu">
+                                        <?php foreach(Yii::$app->urlManager->languages as $language): ?>
+                                        <li>
+                                            <?= Html::a($language,['',Yii::$app->urlManager->languageParam=>$language]) ?>
+                                        </li>
+                                        <?php endforeach; ?>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
+                        <!-- User Account: style can be found in dropdown.less -->
+                        <li class="dropdown user user-menu">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                <i class="glyphicon glyphicon-user"></i>
+                                <span><?= \Yii::$app->user->identity->username ?> <i class="caret"></i></span>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <!-- User image -->
+                                <li class="user-header bg-light-blue">
+                                    <?php echo \cebe\gravatar\Gravatar::widget(
+                                        [
+                                            'email'   => (\Yii::$app->user->identity->profile->gravatar_email === null)
+                                                        ? \Yii::$app->user->identity->email 
+                                                        : \Yii::$app->user->identity->profile->gravatar_email,
+                                            'options' => [
+                                                'alt' => \Yii::$app->user->identity->username
+                                            ],
+                                            'size'    => 128
+                                        ]
+                                    ); ?>
+                                    <p>
+                                        <?= \Yii::$app->user->identity->username ?>
+                                        <small><?= \Yii::$app->user->identity->email ?></small>
+                                    </p>
+                                </li>
+                                <!-- Menu Footer-->
+                                <li class="user-footer">
+                                    <div class="pull-left">
+                                        <a href="<?= \yii\helpers\Url::to(['/user/settings/profile']) ?>"
+                                           class="btn btn-default btn-flat">Profile</a>
+                                    </div>
+                                    <div class="pull-right">
+                                        <a href="<?= \yii\helpers\Url::to(['/user/security/logout']) ?>"
+                                           class="btn btn-default btn-flat" data-method="post">Sign out</a>
+                                    </div>
+                                </li>
+                            </ul>
+                        </li>
+                    <?php endif; ?>
+                </ul>
+            </div>
+        </nav>
+    </header>
+    <!-- Left side column. contains the logo and sidebar -->
+    <aside class="main-sidebar">
+        <!-- sidebar: style can be found in sidebar.less -->
+        <section class="sidebar">
+            <?= $this->render('_sidebar') ?>
+        </section>
+        <!-- /.sidebar -->
+    </aside>
+
+    <!-- Right side column. Contains the navbar and content of the page -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+            <h1>
+                <small><?= $this->title ?></small>
+            </h1>
+            <ol class="breadcrumb">
+                <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+                <li class="active">Dashboard</li>
+            </ol>
+        </section>
+
+        <!-- Main content -->
+
+        <section class="content">
+            <?= Alert::widget() ?>
+            <?= $content ?>
+        </section>
+        <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+    <footer class="main-footer">
+        Powered by <strong><a href="http://phundament.com">Phundament 4</a></strong>
+    </footer>
+</div>
+<!-- ./wrapper -->
+
+<?php $this->endBody() ?>
+</body>
+</html>
+<?php $this->endPage() ?>
