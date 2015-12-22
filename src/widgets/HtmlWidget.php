09b4b663cf50d49d1b701f6815d95a95e54abe1a
Tobias Munk <schmunk@usrbin.de>
2015-12-22 22:14:01 +0100

    replace frontend editing in HtmlWidget with flash message

--- src/widgets/HtmlWidget.php
+++ src/widgets/HtmlWidget.php
@@ -21,44 +21,18 @@ class HtmlWidget extends Widget
     {
         $html = \Yii::$app->settings->get($this->generateKey(), self::SETTINGS_SECTION, null);
         if (\Yii::$app->user->can(self::ACCESS_ROLE)) {
-            $label = ' <span class="label label-info">'.$this->generateKey().'</span> ';
-            $label .= ' <span class="label label-default">'.self::SETTINGS_SECTION.'</span> ';
-            if (!$html) {
-                $buttons = $this->generateCreateLink();
-                $buttons .= $this->generateEditLink();
-            } else {
-                $buttons = $this->generateEditLink();
-            }
-            $code = '<div class="">'.$buttons.$label.'</div>'.$html;
-        } else {
-            $code = $html;
+            $settingsModule = Html::a('settings module', ['/settings']);
+            \Yii::$app->session->addFlash(
+                ($html) ? 'success' : 'info',
+                "Edit contents in {$settingsModule}, section: <code>".self::SETTINGS_SECTION."</code> key: <code>{$this->generateKey()}</code>"
+            );
         }
-
-        return $code;
-    }
-
-    private function generateCreateLink()
-    {
-        return Html::a(
-            '<i class="glyphicon glyphicon-plus"></i>',
-            ['/settings/default/create'],
-            ['class' => 'btn btn-default']
-        );
-    }
-
-    private function generateEditLink()
-    {
-        return Html::a(
-            '<i class="glyphicon glyphicon-wrench"></i>',
-            ['/settings/default'],
-            ['class' => 'btn btn-default']
-        );
+        return $html;
     }
 
     private function generateKey()
     {
         $id = \Yii::$app->request->getQueryParam('id');
-
         return \Yii::$app->language.'/'.\Yii::$app->controller->route.($id ? '/'.$id : '');
     }
 }
