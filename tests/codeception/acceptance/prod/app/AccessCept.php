1076316cfcacb6eb6638481088429118d78489c6
Tobias Munk <schmunk@usrbin.de>
2015-12-26 19:50:18 +0100

    hardend access cept

--- tests/codeception/acceptance/prod/app/AccessCept.php
+++ tests/codeception/acceptance/prod/app/AccessCept.php
@@ -5,12 +5,19 @@ use tests\codeception\_pages\LoginPage;
 $I = new AcceptanceTester($scenario);
 $I->wantTo('ensure backend access works');
 
+$I->amOnPage('/backend');
+$I->dontSeeElement('.small-box');
 $I->amOnPage('/cms/html');
 $I->dontSee('Htmls', 'h1');
-$I->makeScreenshot('access-cms-html');
+$I->amOnPage('/cms/less');
+$I->dontSee('Lesses', 'h1');
 
 $loginPage = LoginPage::openBy($I);
 $loginPage->login('admin', 'admin');
 
+$I->amOnPage('/backend');
+$I->seeElement('.small-box');
 $I->amOnPage('/cms/html');
 $I->see('Htmls', 'h1');
+$I->amOnPage('/cms/less');
+$I->see('Lesses', 'h1');
