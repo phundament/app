a1f4b4f3b7b093f010e0d0d0507974a50a7b5d5f
Tobias Munk <schmunk@usrbin.de>
2015-12-29 03:21:09 +0100

    fixed giiant-batch script

--- src/giiant-batch.sh
+++ src/giiant-batch.sh
@@ -7,16 +7,16 @@ mkdir -p /app/src/extensions/${MODULE_ID}/models/search
 
 # Note: use --modelDb=db_${MODULE_ID} for module_specific db connections
 
-./yii giiant-batch \
-    --interactive=0 \
-    --overwrite=1 \
-    --enableI18N=1 \
-    --messageCategory=app \
-    --modelNamespace=ext\\${MODULE_ID}\\models \
-    --modelBaseClass=yii\\db\\ActiveRecord \
-    --modelBaseTraits=\\ext\\${MODULE_ID}\\traits\\ActiveRecordDbConnectionTrait \
-    --crudControllerNamespace=ext\\${MODULE_ID}\\controllers \
-    --crudSearchModelNamespace=ext\\${MODULE_ID}\\models\\search \
-    --crudViewPath=@ext/${MODULE_ID}/views \
-    --crudPathPrefix= \
-    --crudProviders=schmunk42\\giiant\\generators\\crud\\providers\\DateTimeProvider
+yii giiant-batch \
+      --interactive=0 \
+      --overwrite=1 \
+      --tablePrefix=app_ \
+      --modelDb=db \
+      --modelNamespace=app\\modules\\cms\\models \
+      --modelQueryNamespace=app\\modules\\cms\\models\\query \
+      --crudAccessFilter=1 \
+      --crudControllerNamespace=app\\modules\\cms\\controllers \
+      --crudSearchModelNamespace=app\\modules\\cms\\models\\search \
+      --crudViewPath=@app/modules/cms/views \
+      --crudPathPrefix= \
+      --tables=app_html,app_less
