7bd57b9a38c8ffb703a6a76cde31f70275c8a9e2
Tobias Munk <schmunk@usrbin.de>
2015-12-29 03:20:23 +0100

    fixed api controller behaviors

--- src/modules/cms/controllers/api/LessController.php
+++ src/modules/cms/controllers/api/LessController.php
@@ -5,7 +5,31 @@ namespace app\modules\cms\controllers\api;
 /**
  * This is the class for REST controller "LessController".
  */
+
+use yii\filters\AccessControl;
+use yii\helpers\ArrayHelper;
+
 class LessController extends \yii\rest\ActiveController
 {
     public $modelClass = 'app\modules\cms\models\Less';
+    /**
+    * @inheritdoc
+    */
+    public function behaviors()
+    {
+        return ArrayHelper::merge(
+            parent::behaviors(),
+            [
+                'access' => [
+                    'class' => AccessControl::className(),
+                    'rules' => [
+                        [
+                            'allow' => true,
+                            'matchCallback' => function ($rule, $action) {return \Yii::$app->user->can($this->module->id . '_' . $this->id . '_' . $action->id, ['route' => true]);},
+                        ]
+                    ]
+                ]
+            ]
+        );
+    }
 }
