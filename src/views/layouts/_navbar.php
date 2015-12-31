fcce80e1c06ac664c40c296042d6f7080ff72a8a
Tobias Munk <schmunk@usrbin.de>
2015-12-31 02:05:37 +0100

    created subview for navbar

---
+++ src/views/layouts/_navbar.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace app\views\layouts;
+
+use yii\bootstrap\Nav;
+use yii\bootstrap\NavBar;
+
+$menuItems = [];
+
+if (\Yii::$app->hasModule('user')) {
+    if (\Yii::$app->user->isGuest) {
+        #$menuItems[] = ['label' => 'Signup', 'url' => ['/user/registration/register']];
+        $menuItems[] = ['label' => 'Login', 'url' => ['/user/security/login']];
+    } else {
+        $menuItems[] = [
+            'label' => '<i class="glyphicon glyphicon-user"></i> '.\Yii::$app->user->identity->username,
+            'options' => ['id' => 'link-user-menu'],
+            'items' => [
+                [
+                    'label' => '<i class="glyphicon glyphicon-user"></i> Profile',
+                    'url' => ['/user/profile/show', 'id' => \Yii::$app->user->id],
+                ],
+                '<li class="divider"></li>',
+                [
+                    'label' => '<i class="glyphicon glyphicon-log-out"></i> Logout',
+                    'url' => ['/user/security/logout'],
+                    'linkOptions' => ['data-method' => 'post', 'id' => 'link-logout'],
+                ],
+            ],
+        ];
+        $menuItems[] = [
+            'label' => '<i class="glyphicon glyphicon-cog"></i>',
+            'url' => ['/backend'],
+            'visible' => \Yii::$app->user->can(
+                    'backend_default'
+                ) || (isset(\Yii::$app->user->identity) && \Yii::$app->user->identity->isAdmin),
+        ];
+    }
+}
+
+NavBar::begin(
+    [
+        'brandLabel' => getenv('APP_TITLE'),
+        'brandUrl' => \Yii::$app->homeUrl,
+        'options' => [
+            'class' => 'navbar navbar-top',
+        ],
+    ]
+);
+echo Nav::widget(
+    [
+        'options' => ['class' => 'navbar-nav'],
+        'encodeLabels' => false,
+        'items' => \dmstr\modules\pages\models\Tree::getMenuItems('root_'.\Yii::$app->language),
+    ]
+);
+
+echo Nav::widget(
+    [
+        'options' => ['class' => 'navbar-nav navbar-right'],
+        'encodeLabels' => false,
+        'items' => $menuItems,
+    ]
+);
+
+NavBar::end();
+
+?>
--- src/views/layouts/main.php
+++ src/views/layouts/main.php
@@ -37,67 +37,8 @@ switch (Yii::$app->settings->get('registerPrototypeAsset', 'app.assets')) {
 <?php $this->beginBody() ?>
 <div class="wrap">
 
-    <?php
-    NavBar::begin(
-        [
-            'brandLabel' => getenv('APP_TITLE'),
-            'brandUrl' => Yii::$app->homeUrl,
-            'options' => [
-                'class' => 'navbar-default',
-            ],
-        ]
-    );
-    echo Nav::widget(
-        [
-            'options' => ['class' => 'navbar-nav'],
-            'encodeLabels' => false,
-            'items' => \dmstr\modules\pages\models\Tree::getMenuItems('root_'.Yii::$app->language),
-        ]
-    );
-    $menuItems = [];
-    if (Yii::$app->hasModule('user')) {
-        if (Yii::$app->user->isGuest) {
-            $menuItems[] = ['label' => 'Signup', 'url' => ['/user/registration/register']];
-            $menuItems[] = ['label' => 'Login', 'url' => ['/user/security/login']];
-        } else {
-            $menuItems[] = [
-                'label' => '<i class="glyphicon glyphicon-user"></i> '.Yii::$app->user->identity->username,
-                'options' => ['id' => 'link-user-menu'],
-                'items' => [
-                    [
-                        'label' => '<i class="glyphicon glyphicon-user"></i> Profile',
-                        'url' => ['/user/profile/show', 'id' => \Yii::$app->user->id],
-                    ],
-                    '<li class="divider"></li>',
-                    [
-                        'label' => '<i class="glyphicon glyphicon-log-out"></i> Logout',
-                        'url' => ['/user/security/logout'],
-                        'linkOptions' => ['data-method' => 'post', 'id' => 'link-logout'],
-                    ],
-                ],
-            ];
-            $menuItems[] = [
-                'label' => '<i class="glyphicon glyphicon-cog"></i>',
-                'url' => ['/backend'],
-                'visible' => Yii::$app->user->can(
-                        'backend_default'
-                    ) || (isset(Yii::$app->user->identity) && Yii::$app->user->identity->isAdmin),
-            ];
-        }
-    }
-    echo Nav::widget(
-        [
-            'options' => ['class' => 'navbar-nav navbar-right'],
-            'encodeLabels' => false,
-            'items' => $menuItems,
-        ]
-    );
-    NavBar::end();
-    ?>
-
-    <div class="alert-wrapper">
-        <?= Alert::widget() ?>
-    </div>
+    <?= $this->render('_navbar') ?>
+    <div class="alert-wrapper"><?= Alert::widget() ?></div>
 
     <?= $content ?>
 </div>
