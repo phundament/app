10e3c1c78262affed8f9bf4ac6bad90c474df7ea
Tobias Munk <schmunk@usrbin.de>
2016-01-12 14:26:23 +0100

    fixed NS

--- src/components/Helper.php
+++ src/components/Helper.php
@@ -1,4 +1,7 @@
 <?php
+
+namespace app\components;
+
 /**
  * @link http://www.diemeisterei.de/
  *
@@ -7,7 +10,6 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-namespace app\components;
 
 use dektrium\user\models\User as UserModel;
 use yii\base\Component;
