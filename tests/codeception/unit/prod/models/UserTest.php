c78a55931e2ef065e389876aeb3a8a3237438599
Tobias Munk <schmunk@usrbin.de>
2016-03-03 19:17:25 +0100

    updated user unit tests

--- tests/codeception/unit/prod/models/UserTest.php
+++ tests/codeception/unit/prod/models/UserTest.php
@@ -2,6 +2,7 @@
 
 namespace tests\codeception\unit\models;
 
+use dektrium\user\models\User;
 use yii\codeception\TestCase;
 
 class UserTest extends TestCase
@@ -12,9 +13,22 @@ class UserTest extends TestCase
     protected function setUp()
     {
         parent::setUp();
-        // uncomment the following to load fixtures for user table
-        //$this->loadFixtures(['user']);
     }
 
-    // TODO add test methods here
+    public function testUserLogout()
+    {
+        $this->assertTrue(\Yii::$app->user->logout());
+    }
+
+    public function testUserLogin()
+    {
+        $identity = User::findIdentity(1);
+        $this->assertTrue(\Yii::$app->user->login($identity, 3600));
+    }
+
+    public function testNonExistingUserModel()
+    {
+        $identity = User::findIdentity(99999);
+        $this->assertNull($identity);
+    }
 }
