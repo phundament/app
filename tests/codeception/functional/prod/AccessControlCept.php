90b548a604d00f73c967cef93bd9da4fc4bb40b8
Tobias Munk <schmunk@usrbin.de>
2016-03-04 19:58:06 +0100

    added access control test

---
+++ tests/codeception/functional/prod/AccessControlCept.php
@@ -0,0 +1,21 @@
+<?php
+
+$I = new FunctionalTester($scenario);
+$I->wantTo('ensure that access control works');
+
+$routesWithAccessControl = [
+    '/backend',
+    '/backend/default/view-config',
+    '/prototype',
+    '/prototype/html',
+    '/settings',
+    '/translatemanager',
+    '/rbac',
+    '/user',
+    '/pages'
+];
+
+foreach ($routesWithAccessControl AS $route) {
+    $I->amOnPage($route);
+    $I->canSeeCurrentUrlMatches('|user/login|');
+}
