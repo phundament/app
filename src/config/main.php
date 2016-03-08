07924c2bcc173aba263b756c061b3983550f1d45
Tobias Munk <schmunk@usrbin.de>
2016-03-08 19:46:32 +0100

    removed config/bootstrap.php; moved content to main config

--- src/config/main.php
+++ src/config/main.php
@@ -1,5 +1,10 @@
 <?php
 
+Yii::setAlias('@app', dirname(__DIR__).'/..');
+Yii::setAlias('@runtime', dirname(__DIR__).'/../../runtime');
+Yii::setAlias('@web', dirname(__DIR__).'/../web');
+Yii::setAlias('@webroot', dirname(__DIR__).'/web');
+Yii::setAlias('@root', '@app');
 
 $common = [
     'id' => 'app',
