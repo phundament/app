869db1f86bda9e4fc06c08ce0549f63c30f6c5ce
Tobias Munk <schmunk@usrbin.de>
2016-03-24 19:41:17 +0100

    updated tests

--- tests/codeception/acceptance/prod/app/HomeCept.php
+++ tests/codeception/acceptance/prod/app/HomeCept.php
@@ -4,6 +4,6 @@ $I = new AcceptanceTester($scenario);
 
 $I->wantTo('ensure that home page is not visible without login');
 $I->amOnPage(Yii::$app->homeUrl);
-$I->dontSee(getenv('APP_NAME'));
+$I->dontSee('Sign in', '.panel.panel-default');
 $I->seeLink('Login');
 $I->makeScreenshot('home');
