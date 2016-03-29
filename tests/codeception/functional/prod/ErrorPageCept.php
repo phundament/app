a083499a61d2a0ae18fc68e3d38945e207c72395
Tobias Munk <schmunk@usrbin.de>
2016-03-29 21:05:36 +0200

    added error page test

---
+++ tests/codeception/functional/prod/ErrorPageCept.php
@@ -0,0 +1,8 @@
+<?php
+
+$I = new FunctionalTester($scenario);
+$I->wantTo('ensure that error page works');
+
+$I->amOnPage('/_this_page_does_not_exist_');
+$I->seeResponseCodeIs(404);
+$I->see('Not Found');
