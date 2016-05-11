c7be21fbd524850790b552bb60ca54a5f7da6a2a
Tobias Munk <schmunk@usrbin.de>
2016-05-11 13:46:33 +0200

    updated navbar for new pages version

--- src/views/layouts/_navbar.php
+++ src/views/layouts/_navbar.php
@@ -49,10 +49,23 @@ echo Nav::widget(
     [
         'options' => ['class' => 'navbar-nav'],
         'encodeLabels' => false,
-        'items' => \dmstr\modules\pages\models\Tree::getMenuItems('root_'.\Yii::$app->language),
+        'items' => \dmstr\modules\pages\models\Tree::getMenuItems('root'),
     ]
 );
-
+?>
+    <ul class="nav navbar-nav pull-right">
+        <li class="dropdown" id="menuLogin">
+            <a class="dropdown-toggle" href="#" data-toggle="dropdown" id="navLogin">Login</a>
+            <div class="dropdown-menu" style="padding:17px;">
+                <form class="form" id="formLogin">
+                    <input name="username" id="username" type="text" placeholder="Username">
+                    <input name="password" id="password" type="password" placeholder="Password"><br>
+                    <button type="button" id="btnLogin" class="btn">Login</button>
+                </form>
+            </div>
+        </li>
+    </ul>
+<?php
 echo Nav::widget(
     [
         'options' => ['class' => 'navbar-nav navbar-right'],
@@ -60,5 +73,8 @@ echo Nav::widget(
         'items' => $menuItems,
     ]
 );
+
+
+
 
 NavBar::end();
